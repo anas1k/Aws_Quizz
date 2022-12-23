d3a758eb0a4ae11faa1125c74bd2ba3cc5e92e43
anas1k <anass.elk.48@gmail.com>
2022-12-23 15:38:35 +0100

    AQ-2 stepper done with css

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
     </head>
     <body>
         <main class="wallpaper">
@@ -21,7 +22,7 @@
                     <li>Result</li>
                 </ul>
             </div>
-            <!-- <div class="quizz-card">
+            <div class="quizz-card">
                 <div class="question">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus explicabo excepturi repudiandae? Quod delectus eaque similique omnis incidunt ea?</div>
                 <section class="answers">
                     <div class="card" data-answer="1">Answer 1</div>
@@ -29,7 +30,7 @@
                     <div class="card" data-answer="3">Answer 1</div>
                     <div class="card" data-answer="4">Answer 1</div>
                 </section>
-            </div> -->
+            </div>
         </main>
     </body>
 </html>
