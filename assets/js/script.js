a99590a857bfc8bed8ce70f3f5073469ab78eda7
anas1k <anass.elk.48@gmail.com>
2023-01-02 17:16:30 +0100

    AQ-6 Justification done

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ var questions = [
         C: 'Amazon EC2 instances can be launched on demand when needed',
         D: 'Users can permanently run enough instances to handle peak workloads.',
         correct: 'C',
+        justification:
+            'The ability to launch instances on demand when needed allows users to launch and terminate instances in response to a varying workload. This is a more economical practice than purchasing enough on-premises servers to handle the peak load',
     },
     {
         question: 'Which AWS service would simplify the migration of a database to AWS?',
@@ -15,6 +17,8 @@ var questions = [
         C: 'Amazon EC2',
         D: 'Amazon AppStream 2.0',
         correct: 'B',
+        justification:
+            'AWS DMS helps users migrate databases to AWS quickly and securely. The source database remains fully operational during the migration, minimizing downtime to applications that rely on the database. AWS DMS can migrate data to and from most widely used commercial and open-source databases.',
     },
     {
         question: ' Which AWS offering enables users to find, buy, and immediately start using software solutions in their AWS environment?',
@@ -23,6 +27,7 @@ var questions = [
         C: 'AWS SDK',
         D: 'AWS Marketplace',
         correct: 'D',
+        justification: 'AWS Marketplace is a digital catalog with thousands of software listings from independent software vendors that makes it easy to find, test, buy, and deploy software that runs on AWS.',
     },
     {
         question: 'Which AWS networking service enables a company to create a virtual network within AWS?',
@@ -31,6 +36,7 @@ var questions = [
         C: 'AWS Direct Connect',
         D: 'Amazon Virtual Private Cloud (Amazon VPC)',
         correct: 'D',
+        justification: 'Amazon VPC lets users provision a logically isolated section of the AWS Cloud where users can launch AWS resources in a virtual network that they define.',
     },
     {
         question: 'Which of the following is an AWS responsibility under the AWS shared responsibility model?',
@@ -39,6 +45,7 @@ var questions = [
         C: 'Securing application access and data',
         D: 'Managing guest operating systems',
         correct: 'B',
+        justification: 'Maintaining physical hardware is an AWS responsibility under the AWS shared responsibility model.',
     },
     {
         question: 'Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latency delivery?',
@@ -47,6 +54,7 @@ var questions = [
         C: 'Availability Zones',
         D: 'Virtual Private Cloud (VPC)',
         correct: 'B',
+        justification: 'To deliver content to users with lower latency, Amazon CloudFront uses a global network of points of presence (edge locations and regional edge caches) worldwide. ',
     },
     {
         question: "How would a system administrator add an additional layer of login security to a user's AWS Management Console?",
@@ -55,6 +63,8 @@ var questions = [
         C: 'Enable multi-factor authentication',
         D: 'Enable AWS CloudTrail',
         correct: 'C',
+        justification:
+            'Multi-factor authentication (MFA) is a simple best practice that adds an extra layer of protection on top of a username and password. With MFA enabled, when a user signs in to an AWS Management Console, they will be prompted for their username and password (the first factor—what they know), as well as for an authentication code from their MFA device (the second factor—what they have). Taken together, these multiple factors provide increased security for AWS account settings and resources.',
     },
     {
         question: 'Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?',
@@ -63,6 +73,8 @@ var questions = [
         C: 'AWS X-Ray',
         D: 'AWS Identity and Access Management (AWS IAM)',
         correct: 'B',
+        justification:
+            'AWS CloudTrail helps users enable governance, compliance, and operational and risk auditing of their AWS accounts. Actions taken by a user, role, or an AWS service are recorded as events in CloudTrail. Events include actions taken in the AWS Management Console, AWS Command Line Interface (CLI), and AWS SDKs and APIs.',
     },
     {
         question: 'Which service would be used to send alerts based on Amazon CloudWatch alarms?',
@@ -71,6 +83,8 @@ var questions = [
         C: 'AWS Trusted Advisor',
         D: 'Amazon Route 53',
         correct: 'A',
+        justification:
+            'Amazon SNS and Amazon CloudWatch are integrated so users can collect, view, and analyze metrics for every active SNS. Once users have configured CloudWatch for Amazon SNS, they can gain better insight into the performance of their Amazon SNS topics, push notifications, and SMS deliveries.',
     },
     {
         question: 'Where can a user find information about prohibited actions on the AWS infrastructure?',
@@ -79,6 +93,7 @@ var questions = [
         C: 'AWS Billing Console',
         D: 'AWS Acceptable Use Policy',
         correct: 'D',
+        justification: 'The AWS Acceptable Use Policy provides information regarding prohibited actions on the AWS infrastructure. ',
     },
 ];
 
@@ -189,13 +204,38 @@ function countdown() {
     }, 1000);
 }
 
-// show ansaers when questions are finished
+// show answers when questions are finished
 function showAnswers() {
-    for (let i = 0; i <= questions.length; i++) {
-        document.getElementById('question').innerHTML = questions[i].question;
-        document.getElementById('option1').innerText = questions[i].A;
-        document.getElementById('option2').innerText = questions[i].B;
-        document.getElementById('option3').innerText = questions[i].C;
-        document.getElementById('option4').innerText = questions[i].D;
+    for (let i = 0; i < questions.length; i++) {
+        if (user_answers[i] != questions[i].correct) {
+            document.querySelector('.justify').innerHTML += `
+                <div class="question" id="question">Question : ${questions[i].question}</div>
+                <section class="answers">
+                    <div  class="option" id="A${i}">${questions[i].A}</div>
+                    <div class="option" id="B${i}">${questions[i].B}</div>
+                    <div class="option" id="C${i}">${questions[i].C}</div>
+                    <div class="option" id="D${i}">${questions[i].D}</div>
+                    <div class=" justification" id="justif">Justification : ${questions[i].justification}</div>
+                </section>
+            `;
+            if (user_answers[i] == 'A') {
+                document.querySelector(`#A${i}`).setAttribute('class', 'option false');
+            } else if (user_answers[i] == 'B') {
+                document.querySelector(`#B${i}`).setAttribute('class', 'option false');
+            } else if (user_answers[i] == 'C') {
+                document.querySelector(`#C${i}`).setAttribute('class', 'option false');
+            } else if (user_answers[i] == 'D') {
+                document.querySelector(`#D${i}`).setAttribute('class', 'option false');
+            }
+            if (questions[i].correct == 'A') {
+                document.querySelector(`#A${i}`).setAttribute('class', 'option true');
+            } else if (questions[i].correct == 'B') {
+                document.querySelector(`#B${i}`).setAttribute('class', 'option true');
+            } else if (questions[i].correct == 'C') {
+                document.querySelector(`#C${i}`).setAttribute('class', 'option true');
+            } else if (questions[i].correct == 'D') {
+                document.querySelector(`#D${i}`).setAttribute('class', 'option true');
+            }
+        }
     }
 }
